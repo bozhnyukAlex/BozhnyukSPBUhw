1413f97aaf30ff4066805dc1ae8f7b6784c60daa
bozhnyukAlex <bozhnyuks@mail.ru>
2019-09-19 01:21:12 +0300

    Добавлен файл с приветствующим вводом и выводом

---
+++ HW #3/HW.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int option = -1;
+    while(1){
+        printf("Hello, User! Select option. Press number from 1 to 12\n");
+        printf(" 1 - bitAnd\n 2 - bitXor\n 3 - thirdBits\n 4 - fitsBits\n 5 - sign\n 6 - getByte\n 7 - logicalShift\n 8 - addOk\n 9 - bang\n 10 - conditional\n 11 - isPower2\n 12 - Exit Program");
+        printf("\nYour option: ");
+        scanf("%d", &option);
+
+        switch(option){
+            case 1:{
+                break;
+            }
+            case 2:{
+
+                break;
+            }
+            case 3:{
+                break;
+            }
+            case 4:{
+                break;
+            }
+            case 5:{
+                break;
+            }
+            case 6:{
+                break;
+            }
+            case 7:{
+                break;
+            }
+            case 8:{
+                break;
+            }
+            case 9:{
+                break;
+            }
+            case 10:{
+                break;
+            }
+            case 11:{
+                break;
+            }
+            case 12:{
+                return 0;
+                break;
+            }
+
+
+        }
+    }
+
+    return 0;
+}
