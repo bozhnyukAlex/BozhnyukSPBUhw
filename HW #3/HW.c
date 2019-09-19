b97c948cb73b6b65ff4307bf718b6fefab315966
bozhnyukAlex <bozhnyuks@mail.ru>
2019-09-19 18:50:09 +0300

    Реализованы функции bitAnd() и bitXor()

--- HW #3/HW.c
+++ HW #3/HW.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
-#include <string.h>
+
+int bitAnd();
+int bitOr();
+int bitXor();
+
 
 int main(){
     int option = -1;
+    printf("Hello, User! Select option. Press number from 1 to 12\n");
+    printf(" 1 - bitAnd\n 2 - bitXor\n 3 - thirdBits\n 4 - fitsBits\n 5 - sign\n 6 - getByte\n 7 - logicalShift\n 8 - addOk\n 9 - bang\n 10 - conditional\n 11 - isPower2\n 12 - Exit Program");
     while(1){
-        printf("Hello, User! Select option. Press number from 1 to 12\n");
-        printf(" 1 - bitAnd\n 2 - bitXor\n 3 - thirdBits\n 4 - fitsBits\n 5 - sign\n 6 - getByte\n 7 - logicalShift\n 8 - addOk\n 9 - bang\n 10 - conditional\n 11 - isPower2\n 12 - Exit Program");
         printf("\nYour option: ");
         scanf("%d", &option);
-
         switch(option){
             case 1:{
+                printf("Input two numbers: ");
+                int parA, parB, res;
+                scanf("%d %d", &parA, &parB);
+                res = bitAnd(parA, parB);
+                printf("Result: %d \n", &res);
                 break;
             }
             case 2:{
-
+                printf("Input two numbers: ");
+                int parA, parB, res;
+                scanf("%d %d", &parA, &parB);
+                res = bitXor(parA, parB);
+                printf("Result: %d \n", &res);
                 break;
             }
             case 3:{
@@ -54,4 +66,14 @@ int main(){
     }
 
     return 0;
+}
+
+int bitAnd(int parA, int parB){
+    return ~(~parA | ~parB);
+}
+int bitOr(int parA, int parB){
+    return ~(~parA & ~parB);
+}
+int bitXor(int parA, int parB){
+    return bitOr(parA & ~parB, ~parA & parB);
 }
