b8e173ab7434cd56367ffcc6d7515383afc2aaa9
Alexander <bozhnyuks@mail.ru>
2019-10-04 17:29:04 +0300

    Версия для проверки задания про строки

---
+++ HW #4/HW4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int strlen(char* str){
+    size_t strlength = 0;
+    int index = 0;
+    while(str[index]){
+        strlength++;
+        index++;
+    }
+    return strlength;
+}
+void strcpy(char* dst, char* src){
+    int index = 0;
+    while(src[index]){
+        dst[index] = src[index];
+        index++;
+    }
+    dst[index] = '\0';
+}
+char* strcat(char* dst, char* src){
+    size_t srcLen = strlen(src),
+        dstLen = strlen(dst);
+    int index = 0;
+    for(index = 0; index < srcLen; index++){
+        dst[dstLen+index] = src[index];
+    }
+    dst[dstLen+index] = '\0';
+    return dst;
+}
+int strcmp(char* s1, char* s2){
+    int index = 0;
+    while(s1[index] == s2[index] && s1[index] != '\0' && s2[index] != '\0'){
+        index++;
+    }
+    return s1[index] - s2[index];
+}
+/*
+int main(){
+    const size_t MAX_STRING_SIZE = 255;
+    char s1[MAX_STRING_SIZE], s2[MAX_STRING_SIZE];
+    scanf("%s", &s1);
+    scanf("%s", &s2);
+    printf("%i\n", strcmp(s2,s1));
+    return 0;
+}*/
