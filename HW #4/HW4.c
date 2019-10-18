44e415f7a495b74b1856688e8c6015c99100529c
Alexander <bozhnyuks@mail.ru>
2019-10-18 19:37:44 +0300

    Заменено на while(1)

--- HW #4/HW4.c
+++ HW #4/HW4.c
@@ -2,7 +2,7 @@
 size_t strlen(char* str){
     size_t strlength = 0;
     int index = 0;
-    while(str[index]){
+    while (str[index]) {
         strlength++;
         index++;
     }
@@ -10,17 +10,19 @@ size_t strlen(char* str){
 }
 void strcpy(char* dst, char* src){
     int index = 0;
-    while(src[index]){
+    while (src[index]) {
         dst[index] = src[index];
         index++;
     }
     dst[index] = '\0';
 }
 char* strcat(char* dst, char* src){
-    size_t srcLen = strlen(src),
-        dstLen = strlen(dst);
+    size_t dstLen = strlen(dst);
     int index = 0;
-    while(index < srcLen){
+    while (1) {
+        if (src[index] == '\0') {
+            break;
+        }
         dst[dstLen+index] = src[index];
         index++;
     }
@@ -29,7 +31,7 @@ char* strcat(char* dst, char* src){
 }
 int strcmp(char* s1, char* s2){
     int index = 0;
-    while(s1[index] == s2[index] && s1[index] != '\0' && s2[index] != '\0'){
+    while (s1[index] == s2[index] && s1[index] != '\0' && s2[index] != '\0') {
         index++;
     }
     return s1[index] - s2[index];
@@ -38,8 +40,8 @@ int strcmp(char* s1, char* s2){
 int main(){
     const size_t MAX_STRING_SIZE = 255;
     char s1[MAX_STRING_SIZE], s2[MAX_STRING_SIZE];
-    scanf("%s", &s1);
-    scanf("%s", &s2);
-    printf("%i\n", strcmp(s2,s1));
+    gets(s1);
+    gets(s2);
+    puts(strcat(s1,s2));
     return 0;
 }*/
