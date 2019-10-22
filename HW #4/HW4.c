37b1d3053cd08a4b449b83160fb0cd09397482fc
Alexander <bozhnyuks@mail.ru>
2019-10-23 00:00:39 +0300

    Поправки

--- HW #4/HW4.c
+++ HW #4/HW4.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 size_t strlen(char* str){
     size_t strlength = 0;
-    int index = 0;
-    while (str[index]) {
+    while (str[strlength]) {
         strlength++;
-        index++;
     }
     return strlength;
 }
 void strcpy(char* dst, char* src){
-    int index = 0;
+    size_t index = 0;
     while (src[index]) {
         dst[index] = src[index];
         index++;
@@ -18,11 +16,8 @@ void strcpy(char* dst, char* src){
 }
 char* strcat(char* dst, char* src){
     size_t dstLen = strlen(dst);
-    int index = 0;
-    while (1) {
-        if (src[index] == '\0') {
-            break;
-        }
+    size_t index = 0;
+    while (src[index]) {
         dst[dstLen+index] = src[index];
         index++;
     }
@@ -30,7 +25,7 @@ char* strcat(char* dst, char* src){
     return dst;
 }
 int strcmp(char* s1, char* s2){
-    int index = 0;
+    size_t index = 0;
     while (s1[index] == s2[index] && s1[index] != '\0' && s2[index] != '\0') {
         index++;
     }
@@ -39,9 +34,11 @@ int strcmp(char* s1, char* s2){
 /*
 int main(){
     const size_t MAX_STRING_SIZE = 255;
-    char s1[MAX_STRING_SIZE], s2[MAX_STRING_SIZE];
+    char s1[MAX_STRING_SIZE], s2[MAX_STRING_SIZE], s3[MAX_STRING_SIZE];
     gets(s1);
     gets(s2);
+    strcpy(s3,s1);
     puts(strcat(s1,s2));
+    printf("%d\n%d\n%s", strlen(s2), strcmp(s3,s2), s3);
     return 0;
 }*/
