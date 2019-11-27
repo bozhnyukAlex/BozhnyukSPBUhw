35df6afd6a0b59b700cd35df5d975848a1f7a230
Alexander <bozhnyuks@mail.ru>
2019-11-27 16:00:50 +0300

    Динамическое выделение, строки разделяет только \n, согласно условию задачи

--- HW #9 (MAPFILES)/MapSort/main.c
+++ HW #9 (MAPFILES)/MapSort/main.c
@@ -8,8 +8,12 @@
 #define MAX_STR_LEN 100
 #define MAX_STR_CNT 10000
 
+int strCmp (const void * a, const void * b) {
+  return strcmp(*(char**)a, *(char**)b);
+}
 int main() {
-    int fd = open("content.txt", O_RDWR);
+    int fd = open("content.txt", O_RDWR, 0);
+    int fdout = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IWRITE);
     int i = 0, j = 0;
     if (fd == -1) {
         printf("ERROR");
@@ -17,45 +21,87 @@ int main() {
     }
     struct stat st;
 	fstat(fd, &st); ///struct with information about fd (we need size - bytes count)
-	int size = st.st_size;
-    char *text = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-
-
-    int tl = strlen(text);
-    int strCount = 1;
-    for (i = 0; i < tl; i++) {
-        if (text[i] == ' ') {
+	int size = st.st_size, osize = 0;
+    char *text = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    char *res = (char*) malloc(size * sizeof(int));
+   // puts(text);
+    if (text == MAP_FAILED || res == NULL) {
+        printf("ERROR");
+        return 1;
+    }
+    int txtLen = strlen(text);
+    int maxLen = -1, ///for malloc
+        curLen = 0,
+        strCount = 1;
+    for (i = 0; i < txtLen; i++) {
+        if (text[i] != '\n') {
+            curLen++;
+        }
+        else {
+            if (maxLen < curLen) {
+                maxLen = curLen;
+            }
+            osize += (curLen + 1);
+            curLen = 0;
             strCount++;
         }
     }
-    //puts(text);
-    //printf("!!! %d\n", strCount);
-    char strings[MAX_STR_CNT][MAX_STR_LEN];
-    int cur = 0, pos = 0;
-    for (i = 0; i < strCount; i++) {
-        cur = pos;
-        while (text[cur] && text[cur] != ' ' && text[cur] != '\n') {
-            cur++;
+
+   // printf("!!! %d %d\n", strCount, maxLen);
+    char **strings = (char**) malloc(strCount * sizeof(char*));
+    char *cur = (char*) malloc(maxLen * sizeof(char));
+
+    if (strings == NULL) {
+        printf("ERROR");
+        return 1;
+    }
+    int pos = 0;
+    for (i = 0; i < strCount;) {
+        j = 0;
+        for (; text[pos] != '\n' && pos < txtLen; pos++) {
+            cur[j] = text[pos];
+            j++;
         }
-        for (j = pos; j < cur; j++) {
-            strings[i][j - pos] = text[j];
+        cur[j] = '\0';
+        //j++;
+        pos++;
+        strings[i] = strdup(cur);
+        i++;
+        if (pos >= txtLen) {
+            break;
         }
-        strings[i][cur] = '\0';
-        pos = cur + 1;
     }
 
-    qsort(strings, strCount, sizeof(char[MAX_STR_LEN]), (int (*)(const void *,const  void *)) strcmp);
-
-    sprintf(text, "");
-    for (i = 0; i < strCount; i++) {
-        sprintf(text, "%s %s", text, strings[i]);
-    }
-//    puts(text);
 //    for (i = 0; i < strCount; i++) {
 //        puts(strings[i]);
 //    }
+  //  printf("\n");
+    qsort(strings, strCount, sizeof(char*), strCmp);
+
+//    for (i = 0; i < strCount; i++) {
+//        puts(strings[i]);
+//    }
+    sprintf(res, "");
+    for (i = 0; i < strCount; i++) {
+        if (i == 0) {
+            sprintf(res, "%s", strings[i]);
+        }
+        else {
+            sprintf(res, "%s\n%s", res, strings[i]);
+        }
+        osize += strlen(strings[i]);
+
+    }
+  //  puts(res);
 
 
+    write(fdout, res, osize);
+    for (i = 0; i < strCount; i++) {
+        free(strings[i]);
+    }
+    free(strings);
+    free(cur);
+    free(res);
     munmap(text, size);
     close(fd);
     return 0;
