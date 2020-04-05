e9c8abb8d6c032e6535546ab57ade31dbe8f211b
AlexBozhnyuk <bozhnyuks@mail.ru>
2020-04-05 22:47:55 +0200

    Первая проба JavaFX

---
+++ SEMESTER#2/BattleShip/src/sample/Controller.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+public class Controller {
+
+//    @FXML
+//    private Button btnYes;
+//    @FXML
+//    private Button btnNo;
+//    @FXML
+//    private Label label;
+//
+//
+//    @FXML
+//    public void initialize() {
+//        btnYes.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                label.setText("*****");
+//            }
+//        });
+//        btnNo.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                label.setText("!!!!!");
+//            }
+//        });
+//    }
+
+
+}
