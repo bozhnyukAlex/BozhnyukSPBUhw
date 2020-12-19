ab908aee08cf5f3d5c7823808dad7e32e7c54c1c
AlexBozhnyuk <bozhnyuks@mail.ru>
2020-12-20 01:31:29 +0300

    hints added

--- EXTRA/MonteCarloMethod/src/com/company/MonteCarlo.java
+++ EXTRA/MonteCarloMethod/src/com/company/MonteCarlo.java
@@ -20,5 +20,30 @@ public class MonteCarlo {
         return 4.0 * threads.parallelStream().map(CountThread::getHits).reduce(0L, Long::sum) / iter;
     }
 
+    /*
+    import scala.util.Random
+    def mcCount(iter: Int): Int = {
+        val randomX = new Random
+        val randomY = new Random
+        var hits = 0
+        for (i <- 0 until iter) {
+            val x = randomX.nextDouble // in [0,1]
+            val y = randomY.nextDouble // in [0,1]
+            if (x*x + y*y < 1) hits= hits + 1
+        }
+        hits
+     }
+    def monteCarloPiSeq(iter: Int): Double = 4.0 * mcCount(iter) / iter
+     */
+
+    /*
+    def monteCarloPiPar(iter: Int): Double = {
+        val ((pi1, pi2), (pi3, pi4)) = parallel(
+                parallel(mcCount(iter/4), mcCount(iter/4)),
+                parallel(mcCount(iter/4), mcCount(iter - 3*(iter/4))))
+        4.0 * (pi1 + pi2 + pi3 + pi4) / iter
+    }
+     */
+
 
 }
